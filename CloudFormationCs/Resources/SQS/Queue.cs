3e97065ff4eb2e76a1f50cf12f7626de25d2701a
jwdonze <jwdonze@users.noreply.github.com>
2015-09-17 14:54:01 -0700

    - Add SQS Properties

--- CloudFormationCs/Resources/SQS/Queue.cs
+++ CloudFormationCs/Resources/SQS/Queue.cs
@@ -3,10 +3,31 @@ using System;
 namespace CloudFormationCs.Resources.SQS
 {
     /// <summary>
-    /// http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-sqs-queues-redrivepolicy.html
+    /// http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-sqs-queues.html
     /// </summary>
     public class Queue : Resource
     {
+        [Required(false)]
+        public int DelaySeconds { get; set; }
+
+        [Required(false)]
+        public int MaximumMessageSize { get; set; }
+
+        [Required(false)]
+        public int MessageRetentionPeriod { get; set; }
+
+        [Required(false)]
+        public String QueueName { get; set; }
+
+        [Required(false)]
+        public int ReceiveMessageWaitTimeSeconds { get; set; }
+
+        [Required(false)]
+        public RedrivePolicy RedrivePolicy { get; set; }
+
+        [Required(false)]
+        public int VisibilityTimeout { get; set; }
+
         public Queue()
             : base()
         {
