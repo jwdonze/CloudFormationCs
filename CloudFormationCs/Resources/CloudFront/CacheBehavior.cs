ab18d458573a2cdcf2d4540a276c38f9a3922feb
jwdonze <jwdonze@users.noreply.github.com>
2017-06-01 11:04:07 -0700

    - New properties for CacheBehavior

--- CloudFormationCs/Resources/CloudFront/CacheBehavior.cs
+++ CloudFormationCs/Resources/CloudFront/CacheBehavior.cs
@@ -7,9 +7,16 @@ namespace CloudFormationCs.Resources.CloudFront
 {
     public class CacheBehavior
     {
+        public string[] AllowedMethods { get; set; }
+        public string[] CachedMethods { get; set; }
+        public bool? Compress { get; set; }
+        public string DefaultTTL { get; set; }
         public ForwardedValues ForwardedValues { get; set; }
+        public string MaxTTL { get; set; }
         public string MinTTL { get; set; }
+        public bool? SmoothStreaming { get; set; }
         public string TargetOriginId { get; set; }
+        public string[] TrustedSigners { get; set; }
         public string ViewerProtocolPolicy { get; set; }
     }
 }
