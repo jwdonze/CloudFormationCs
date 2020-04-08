e20b80bf12a5066e8adcf43d824e05b3bc570004
jwdonze <jwdonze@users.noreply.github.com>
2020-04-07 18:22:08 -0700

    Add WAFv2

---
+++ CloudFormationCs/Resources/WAFv2/WebACLAssociation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CloudFormationCs.Resources.WAFv2
+{
+    /// <summary>
+    ///https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-wafv2-webaclassociation.html
+    /// </summary>
+    public class WebACLAssociation : Resource
+    {
+        public StringRef ResourceArn { get; set; }
+        public StringRef WebACLArn { get; set; }
+
+
+        public WebACLAssociation()
+            : base()
+        {
+        }
+
+        public WebACLAssociation(StringOrEnum resourceIdentifier)
+            : base(resourceIdentifier)
+        {
+        }
+    }
+}
